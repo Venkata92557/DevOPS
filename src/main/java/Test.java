ecd0d6c7a40fe4a1b4b620b3c8141b6ec60abb93
CAMPUS\venkata.ramana <venkatar@interfile.co.za>
2022-09-06 14:37:40 +0200

    06Auf added code in index.html

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("DevOPS Demo Integrating Docker with Jenkins");
+    public static void main(String[] args)
+    System.out.println("DevOPS Demo Integrating Docker with Jenkins");
     }
 }
